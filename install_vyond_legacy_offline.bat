f1cf0dd15c1b661af63dad84be889eb5e1f9f630
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-04-02 22:34:00 -0400

    Update install_vyond_legacy_offline.bat

--- install_vyond_legacy_offline.bat
+++ install_vyond_legacy_offline.bat
@@ -63,7 +63,7 @@ cls
 pushd "..\..\"
 echo Cloning repository from GitHub...
 call Vyond-Legacy-Offline-Installer-main\Vyond-Legacy-Offline-Installer-main\PortableGit\bin\git.exe clone https://github.com/josephanimate2021/Vyond-Legacy-Offline-Beta.git
-pushd Vyond-Legacy-Offline-Beta
+pushd Vyond-Legacy-Offline-Beta\vyond-legacy
 mkdir .git
 echo Vyond Legacy: Offline Beta Has Been Installed
 pause
